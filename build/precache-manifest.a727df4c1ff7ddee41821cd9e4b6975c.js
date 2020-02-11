4aa215c415f2256dc81527b06effa7966c7851f9
ultimecreation <naserbechata@gmail.com>
2020-02-11 17:16:26 +0100

    build for deployment done

---
+++ build/precache-manifest.a727df4c1ff7ddee41821cd9e4b6975c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3294d028375d1f1d0b85a59c5132a4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28102c6119050c85d9e0",
+    "url": "/static/css/main.2884f6a7.chunk.css"
+  },
+  {
+    "revision": "da65dfce01418fa71fe8",
+    "url": "/static/js/2.5574be1a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.5574be1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28102c6119050c85d9e0",
+    "url": "/static/js/main.67758dcc.chunk.js"
+  },
+  {
+    "revision": "fe18ade0de3ac64e2f15",
+    "url": "/static/js/runtime-main.69bd3fe4.js"
+  }
+]);
