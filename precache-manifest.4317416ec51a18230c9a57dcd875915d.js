e700dabaf5d70ee5be08bd4488c7c1019d7a53e0
118454 <u91994573@infongp-es21.clienthosting.eu>
2021-05-12 07:40:54 +0200

    from 1&1

---
+++ precache-manifest.4317416ec51a18230c9a57dcd875915d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30102286990901f8b0517c5c0cdfb051",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4c0c46178ae49629a27",
+    "url": "/static/css/main.2884f6a7.chunk.css"
+  },
+  {
+    "revision": "da65dfce01418fa71fe8",
+    "url": "/static/js/2.5574be1a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.5574be1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4c0c46178ae49629a27",
+    "url": "/static/js/main.d259a85b.chunk.js"
+  },
+  {
+    "revision": "fe18ade0de3ac64e2f15",
+    "url": "/static/js/runtime-main.69bd3fe4.js"
+  }
+]);
